58c81707e00160be10631585f68beb6b81af820e
bartos-github <139777500+bartosz-github@users.noreply.github.com>
2026-08-13 23:23:04 +0100

    1

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
     <title>PL 2026-2027</title>
 </head>
 <body>
+
     <?php
       $host = "localhost";
-      $user = "******";
-      $password = "******";
-      $database = "******";
+      $user = "***********************";
+      $password = "***********************";
+      $database = "***********************";
 
       $connect = new mysqli($host, $user, $password, $database);
       
@@ -38,14 +39,52 @@
       $connect->close();
 
       echo "<br>"."<br>"."<br>"."<br>"."<br>";
+
+
+
       
     ?>
     <ol>
-        <li>db (table from 25-26)</li>
-        <li>table</li>
-        <li>authentication</li>
+        <li>db (table structure from 25-26 season)</li>
+        <li>form </li>
         <li>form 2 update/edit/delete</li>
+        <li>account & authentication </li>
         <li>styles</li>
     </ol>
+
+<!--<form action="update.php" method="post">-->
+
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  Name: <input type="text" name="name">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail:
+  <input type="text" name="email">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website:
+  <input type="text" name="website">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">
+</form>
+
+<!-- Team: <input type="text" name="team"><br>
+Points: <input type="text" name="points"><br> -->
+
+<input type="submit">
+</form>
+
+
+
 </body>
 </html>
