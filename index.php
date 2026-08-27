558042bbca50ea5074420ca691b244ab28e09923
bartos-github <139777500+bartosz-github@users.noreply.github.com>
2026-08-27 19:26:22 +0100

    1

--- index.php
+++ index.php
@@ -20,9 +20,8 @@
         die("Failed to connect with database");
       echo "Database connected!";
 
-      $sql = "SELECT id, team, points  FROM pltable ORDER BY id";
+      $sql = "SELECT id, team, played, points  FROM pltable ORDER BY id";
       $result = $connect->query($sql);
-
 
       echo "<br>";
       echo "<br>";
@@ -30,7 +29,7 @@
       if ($result->num_rows > 0) {
         // Output data of each row
         while($row = $result->fetch_assoc()) {
-          echo "" . $row["id"]. " - " . $row["team"]." [". $row["points"] ."] ". "<br>";
+          echo "" . $row["id"]. " - " . $row["team"]." [". $row["played"] ."] ". " [". $row["points"] ."] "."<br>";
         }
       } else {
         echo "0 results";
@@ -40,61 +39,36 @@
 
       echo "<br>"."<br>"."<br>"."<br>"."<br>";
 
-
-      
     ?>
 
+  <br>
+  ################################################################################<br>
+  ################################################################################<br>
+          <h2>Score</h2>
+          <form action="insert.php" method="POST">
+              <!-- Team one -->
+              Team 1:
+              <input name="team_one" required type="text"/>
 
-<br>
-################################################################################<br>
-################################################################################<br>
-        <h2>Results & Scores</h2>
-        <form action="insert.php" method="POST">
-            <!-- Team one -->
-            Team 1:
-            <input name="team_one" required type="text"/>
+              <!-- Team two -->
+              Team 2:
+              <input name="team_two" required type="text"/>
+              <br/><br/>
 
-            <!-- Team two -->
-            Team 2:
-            <input name="team_two" required type="text"/>
-            <br/><br/>
+              <!-- Score -->
+              Score:
+              <input name="for_team_one" required type="text"/>
+              <input name="for_team_two" required type="text"/>
+              <br/><br/>
 
-            <!-- Result -->
-            <!-- Result:
-            <input name="result" required type="radio" value="team_1_won"/> Team One Won
-            <input name="result" required type="radio" value="team_2_won"/> Team Two Won
-            <input name="result" required type="radio" value="teams_draw"/> Draw
-            <br/><br/> -->
+              <br/><br/>
 
-            <!-- Score -->
-            Score:
-            <input name="for_team_one" required type="text"/>
-            <input name="for_team_two" required type="text"/>
-            <br/><br/>
-
-            <br/><br/>
-
-            <!-- Submit button -->
-            <input type="submit" value="Submit"/>
-        </form>
-<br>
-################################################################################<br>
-################################################################################<br>
-
-
-    <!--<form action="update.php" method="post">-->
-
-
-
-  <!--https://www.w3schools.com/PHP/php_form_required.asp-->
-
-<!-- Team: <input type="text" name="team"><br>
-Points: <input type="text" name="points"><br> -->
-
-<!-- <input type="submit"> -->
-</form>
-
-
+              <!-- Submit button -->
+              <input type="submit" value="Submit"/>
+          </form>
+  <br>
+  ################################################################################<br>
+  ################################################################################<br>
 
 </body>
 </html>
--- insert.php
+++ insert.php
@@ -3,7 +3,6 @@ $servername = "*****";
 $username = "*****;
 $password = "*****";
 $dbname = "*****";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -20,165 +19,156 @@ $team1score = mysqli_real_escape_string($conn, $_POST['for_team_one']);
 $team2score = mysqli_real_escape_string($conn, $_POST['for_team_two']);
 
 
-
-
-
-switch ($team1) {
-    case 'Arsenal':
-	$team1_id = 1;
+switch (true) {
+    case $team1 == 'Arsenal' OR $team1 == 'Ars':	$team1_id = 1;
         break;
-    case 'Aston Villa':
-	$team1_id = 2;
+    case $team1 == 'Aston Villa' OR $team1 == 'Ast':	$team1_id = 2;
         break;
-    case 'Bournemouth':
-	$team1_id = 3;
+    case $team1 == 'Bournemouth' OR $team1 == 'Bou':	$team1_id = 3;
         break;
-    case 'Brentford':
-	$team1_id = 4;
+    case $team1 == 'Brentford' OR $team1 == 'Bre':	$team1_id = 4;
         break;
-    case 'Brighton & Hove Albion':
-	$team1_id = 5;
+    case $team1 == 'Brighton & Hove Albion' OR $team1 == 'Bri':	$team1_id = 5;
         break;
-    case 'Chelsea':
-	$team1_id = 6;
+    case $team1 == 'Chelsea' OR $team1 == 'Che':	$team1_id = 6;
         break;
-    case 'Coventry City':
-	$team1_id = 7;
+    case $team1 == 'Coventry City' OR $team1 == 'Cov':	$team1_id = 7;
         break;
-    case 'Crystal Palace':
-	$team1_id = 8;
+    case $team1 == 'Crystal Palace' OR $team1 == 'Cry':	$team1_id = 8;
         break;
-    case 'Everton':
-	$team1_id = 9;
+    case $team1 == 'Everton' OR $team1 == 'Eve':	$team1_id = 9;
         break;
-    case 'Fulham':
-	$team1_id = 10;
+    case $team1 == 'Fulham' OR $team1 == 'Ful':	$team1_id = 10;
         break;
-    case 'Hull City':
-	$team1_id = 11;
+    case $team1 == 'Hull City' OR $team1 == 'Hul':	$team1_id = 11;
         break;
-    case 'Ipswich Town':
-	$team1_id = 12;
+    case $team1 == 'Ipswich Town' OR $team1 == 'Ips':	$team1_id = 12;
         break;
-    case 'Leeds United':
-	$team1_id = 13;
+    case $team1 == 'Leeds United' OR $team1 == 'Lee':	$team1_id = 13;
         break;
-    case 'Liverpool':
-	$team1_id = 14;
+    case $team1 == 'Liverpool' OR $team1 == 'Liv':	$team1_id = 14;
         break;
-    case 'Manchester City':
-	$team1_id = 15;
+    case $team1 == 'Manchester City' OR $team1 == 'ManC':	$team1_id = 15;
         break;
-    case 'Manchester United':
-	$team1_id = 16;
+    case $team1 == 'Manchester United' OR $team1 == 'ManU':	$team1_id = 16;
         break;
-    case 'Newcastle United':
-	$team1_id = 17;
+    case $team1 == 'Newcastle United' OR $team1 == 'New':	$team1_id = 17;
         break;
-    case 'Nottingham Forest':
-	$team1_id = 18;
+    case $team1 == 'Nottingham Forest' OR $team1 == 'Not':	$team1_id = 18;
         break;
-    case 'Sunderland':
-	$team1_id = 19;
+    case $team1 == 'Sunderland' OR $team1 == 'Sun':	$team1_id = 19;
         break;
-    case 'Tottenham Hotspur':
-	$team1_id = 20;
+    case $team1 == 'Tottenham Hotspur' OR $team1 == 'Tot':	$team1_id = 20;
         break;
+    default:
+       echo "Error!";
 }
 
-switch ($team2) {
-    case 'Arsenal':
-	$team2_id = 1;
+switch (true) {
+    case $team2 == 'Arsenal' OR $team2 == 'Ars':	$team2_id = 1;
         break;
-    case 'Aston Villa':
-	$team2_id = 2;
+    case $team2 == 'Aston Villa' OR $team2 == 'Ast':	$team2_id = 2;
         break;
-    case 'Bournemouth':
-	$team2_id = 3;
+    case $team2 == 'Bournemouth' OR $team2 == 'Bou':	$team2_id = 3;
         break;
-    case 'Brentford':
-	$team2_id = 4;
+    case $team2 == 'Brentford' OR $team2 == 'Bre':	$team2_id = 4;
         break;
-    case 'Brighton & Hove Albion':
-	$team2_id = 5;
+    case $team2 == 'Brighton & Hove Albion' OR $team2 == 'Bri':	$team2_id = 5;
         break;
-    case 'Chelsea':
-	$team2_id = 6;
+    case $team2 == 'Chelsea' OR $team2 == 'Che':	$team2_id = 6;
         break;
-    case 'Coventry City':
-	$team2_id = 7;
+    case $team2 == 'Coventry City' OR $team2 == 'Cov':	$team2_id = 7;
         break;
-    case 'Crystal Palace':
-	$team2_id = 8;
+    case $team2 == 'Crystal Palace' OR $team2 == 'Cry':	$team2_id = 8;
         break;
-    case 'Everton':
-	$team2_id = 9;
+    case $team2 == 'Everton' OR $team2 == 'Eve':	$team2_id = 9;
         break;
-    case 'Fulham':
-	$team2_id = 10;
+    case $team2 == 'Fulham' OR $team2 == 'Ful':	$team2_id = 10;
         break;
-    case 'Hull City':
-	$team2_id = 11;
+    case $team2 == 'Hull City' OR $team2 == 'Hul':	$team2_id = 11;
         break;
-    case 'Ipswich Town':
-	$team2_id = 12;
+    case $team2 == 'Ipswich Town' OR $team2 == 'Ips':	$team2_id = 12;
         break;
-    case 'Leeds United':
-	$team2_id = 13;
+    case $team2 == 'Leeds United' OR $team2 == 'Lee':	$team2_id = 13;
         break;
-    case 'Liverpool':
-	$team2_id = 14;
+    case $team2 == 'Liverpool' OR $team2 == 'Liv':	$team2_id = 14;
         break;
-    case 'Manchester City':
-	$team2_id = 15;
+    case $team2 == 'Manchester City' OR $team2 == 'ManC':	$team2_id = 15;
         break;
-    case 'Manchester United':
-	$team2_id = 16;
+    case $team2 == 'Manchester United' OR $team2 == 'ManU':	$team2_id = 16;
         break;
-    case 'Newcastle United':
-	$team2_id = 17;
+    case $team2 == 'Newcastle United' OR $team2 == 'New':	$team2_id = 17;
         break;
-    case 'Nottingham Forest':
-	$team2_id = 18;
+    case $team2 == 'Nottingham Forest' OR $team2 == 'Not':	$team2_id = 18;
         break;
-    case 'Sunderland':
-	$team2_id = 19;
+    case $team2 == 'Sunderland' OR $team2 == 'Sun':	$team2_id = 19;
         break;
-    case 'Tottenham Hotspur':
-	$team2_id = 20;
+    case $team2 == 'Tottenham Hotspur' OR $team2 == 'Tot':	$team2_id = 20;
         break;
+    default:
+       echo "Error!";
 }
 
 // temp nl2br
-if ($team1score==$team2score) {
-    echo "DRAW \n";
-    //WHERE username = '$user'";
-    $sql1 = "UPDATE `pltable` SET `played` = (`played` + 1), `drawn` = (`drawn` + 1), `for_goals` = (`for_goals` + $team1score), `against_goals` = (`against_goals` + $team2score), `gd` = (`gd` + ($team1score-$team2score)), `points` = (`points` + 1) WHERE id = $team1_id";
-    $sql2 = "UPDATE `pltable` SET `played` = (`played` + 1), `drawn` = (`drawn` + 1), `for_goals` = (`for_goals` + $team2score), `against_goals` = (`against_goals` + $team1score), `gd` = (`gd` + ($team1score-$team2score)), `points` = (`points` + 1) WHERE id = $team2_id";
+if (($team1score==100) AND ($team2score==100)) {
+    echo "CLEARING DATA... .<br>\n";
+
+    $sql = "FOR id IN 1..20
+            DO
+                UPDATE `pltable` SET `MW1` = '';
+                UPDATE `pltable` SET `played` = 0;
+                UPDATE `pltable` SET `won` = 0;
+                UPDATE `pltable` SET `drawn` = 0;
+                UPDATE `pltable` SET `lost` = 0;
+                UPDATE `pltable` SET `for_goals` = 0;
+                UPDATE `pltable` SET `against_goals` = 0;
+                UPDATE `pltable` SET `gd` = 0;
+                UPDATE `pltable` SET `points` = 0;
+            END FOR;
+    ";
 
 } elseif ($team1score>$team2score) {
-    echo "TEAM ONE WON \n";
-    $sql1 = "UPDATE `pltable` SET `played` = (`played` + 1), `won` = (`won` + 1), `for_goals` = (`for_goals` + $team1score), `against_goals` = (`against_goals` + $team2score), `gd` = (`gd` + ($team1score-$team2score)), `points` = (`points` + 3) WHERE id = $team1_id";
-    $sql2 = "UPDATE `pltable` SET `played` = (`played` + 1), `lost` = (`lost` + 1), `for_goals` = (`for_goals` + $team2score), `against_goals` = (`against_goals` + $team1score), `gd` = (`gd` + ($team2score-$team1score)), `points` = (`points` + 0) WHERE id = $team2_id";
+    echo "WINNER - $team1 .<br>\n";
+    echo "WINNER - $team1_id .<br>\n";
+    echo "LOSER - $team2 .<br>\n";
+    echo "LOSER - $team2_id .<br>\n";
+    
 
+    $sql = "FOR X IN 1..1
+            DO
+                UPDATE `pltable` SET `played` = (`played` + 1), `won` = (`won` + 1), `for_goals` = (`for_goals` + $team1score), `against_goals` = (`against_goals` + $team2score), `gd` = (`gd` + ($team1score-$team2score)), `points` = (`points` + 3) WHERE id = $team1_id;
+                UPDATE `pltable` SET `played` = (`played` + 1), `lost` = (`lost` + 1), `for_goals` = (`for_goals` + $team2score), `against_goals` = (`against_goals` + $team1score), `gd` = (`gd` + ($team2score-$team1score)), `points` = (`points` + 0) WHERE id = $team2_id;
+            END FOR;
+    ";
 } elseif ($team1score<$team2score) {
-    echo "TEAM TWO WON \n";
-    $sql1 = "UPDATE `pltable` SET `played` = (`played` + 1), `lost` = (`lost` + 1), `for_goals` = (`for_goals` + $team1score), `against_goals` = (`against_goals` + $team2score), `gd` = (`gd` + ($team1score-$team2score)), `points` = (`points` + 0) WHERE id = $team1_id";
-    $sql2 = "UPDATE `pltable` SET `played` = (`played` + 1), `won` = (`won` + 1), `for_goals` = (`for_goals` + $team2score), `against_goals` = (`against_goals` + $team1score), `gd` = (`gd` + ($team2score-$team1score)), `points` = (`points` + 3) WHERE id = $team2_id";
+    echo "WINNER - $team2 .<br>\n";
+    echo "WINNER - $team2_id .<br>\n";
+    echo "LOSER - $team1 .<br>\n";
+    echo "LOSER - $team1_id .<br>\n";
+    
+    $sql = "FOR X IN 1..1
+            DO
+                UPDATE `pltable` SET `played` = (`played` + 1), `lost` = (`lost` + 1), `for_goals` = (`for_goals` + $team1score), `against_goals` = (`against_goals` + $team2score), `gd` = (`gd` + ($team1score-$team2score)), `points` = (`points` + 0) WHERE id = $team1_id;
+                UPDATE `pltable` SET `played` = (`played` + 1), `won` = (`won` + 1), `for_goals` = (`for_goals` + $team2score), `against_goals` = (`against_goals` + $team1score), `gd` = (`gd` + ($team2score-$team1score)), `points` = (`points` + 3) WHERE id = $team2_id;
+            END FOR;
+    ";
+} elseif ($team1score==$team2score) {
+    echo "DRAW .<br>\n";
+
+    $sql = "FOR X IN 1..1
+            DO
+                UPDATE `pltable` SET `played` = (`played` + 1), `drawn` = (`drawn` + 1), `for_goals` = (`for_goals` + $team1score), `against_goals` = (`against_goals` + $team2score), `gd` = (`gd` + ($team1score-$team2score)), `points` = (`points` + 1) WHERE id = $team1_id;
+                UPDATE `pltable` SET `played` = (`played` + 1), `drawn` = (`drawn` + 1), `for_goals` = (`for_goals` + $team2score), `against_goals` = (`against_goals` + $team1score), `gd` = (`gd` + ($team1score-$team2score)), `points` = (`points` + 1) WHERE id = $team2_id;
+            END FOR;
+    ";
 }
 
-if ($conn->query($sql1) === TRUE) {
-    echo "New record created successfully";
+// Upd_DB
+if ($conn->query($sql) === TRUE) {
+    echo "Database updated.";
 } else {
-    echo "Error: " . $sql1 . "<br>" . $conn->error;
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
-if ($conn->query($sql2) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql2 . "<br>" . $conn->error;
-}
-
 
 // Close the connection
 $conn->close();
